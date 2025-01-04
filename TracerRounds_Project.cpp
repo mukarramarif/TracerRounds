4ab6edcd445cd852a0b52cfca3c130c5aa947a08
Muhammad Mukarram Arif <mukarramarif03@gmail.com>
2025-01-04 13:27:33 -0600

    Add project files.

---
+++ TracerRounds_Project.cpp
@@ -0,0 +1,12 @@
+﻿// TracerRounds_Project.cpp : Defines the entry point for the application.
+//
+
+#include "TracerRounds_Project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
